140129fb736e4de6964c173adbb66183948e00f9
harrymauray05 <harrymaurya05@gmail.com>
2024-02-05 11:33:11 +0530

    testing controller added

--- src/main/java/github/io/iamhariommaurya/ecart/authService/controller/AuthController.java
+++ src/main/java/github/io/iamhariommaurya/ecart/authService/controller/AuthController.java
@@ -9,10 +9,7 @@ import github.io.iamhariommaurya.ecart.authService.service.ISignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/")
@@ -21,6 +18,11 @@ public class AuthController {
     private  ISignupService signupService;
     @Autowired
     private ILoginService loginService;
+
+    @GetMapping(value = "/testing")
+    public ResponseEntity<String> testing(){
+        return ResponseEntity.ok("Hello World");
+    }
     @PostMapping(value = "login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
         System.out.println(loginRequest.toString());
