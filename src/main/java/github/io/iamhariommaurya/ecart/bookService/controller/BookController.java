345df3eb1d54f0677254bb597bbfd0ae59123353
harrymauray05 <harrymaurya05@gmail.com>
2024-02-11 20:20:08 +0530

    Book Service CRUD operation api added

--- src/main/java/github/io/iamhariommaurya/ecart/bookService/controller/BookController.java
+++ src/main/java/github/io/iamhariommaurya/ecart/bookService/controller/BookController.java
@@ -1,21 +1,41 @@
 package github.io.iamhariommaurya.ecart.bookService.controller;
 
+import github.io.iamhariommaurya.ecart.bookService.model.Book;
 import github.io.iamhariommaurya.ecart.bookService.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/v1/book")
+@RequestMapping(value = "/v1")
 public class BookController {
     @Autowired
     BookService bookService;
-
     //Add Book
-    
-    //Delete Book
-    //Update Book
+    @PostMapping(value = "/book")
+    public ResponseEntity<Book> saveBook(@RequestBody Book book){
+        return ResponseEntity.ok(bookService.save(book));
+    }
     //Get Books
+    @GetMapping(value = "/book")
+    public ResponseEntity<List<Book>> getBooks(){
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
     //Get Book by id
-
+    @GetMapping(value = "/book/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable("id") long id){
+        return ResponseEntity.ok(bookService.getBookByid(id));
+    }
+    //Update Book
+    @PutMapping(value = "/book/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable("id") long id,@RequestBody Book book){
+        return ResponseEntity.ok(bookService.updateBookById(id,book));
+    }
+    //Delete Book
+    @DeleteMapping(value = "/book/{id}")
+    public ResponseEntity<String> deleteBookById(@PathVariable("id") long id){
+        return ResponseEntity.ok(bookService.deleteBookByid(id));
+    }
 }
--- src/main/java/github/io/iamhariommaurya/ecart/bookService/service/impl/BookServiceImpl.java
+++ src/main/java/github/io/iamhariommaurya/ecart/bookService/service/impl/BookServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -35,13 +36,23 @@ public class BookServiceImpl implements BookService {
         Optional<Book> book1 = bookRepository.findById(id);
         if(book1.isPresent()){
             Book originalBook = book1.get();
-
+            if(Objects.nonNull(book.getName()) && !"".equalsIgnoreCase(book.getName())){
+                originalBook.setName(book.getName());
+            }
+            if(Objects.nonNull(book.getPrice()) && book.getPrice() > 0){
+                originalBook.setPrice(book.getPrice());
+            }
+            return bookRepository.save(originalBook);
         }
         return null;
     }
 
     @Override
     public String deleteBookByid(long id) {
-        return null;
+        if(bookRepository.findById(id).isPresent()){
+            bookRepository.deleteById(id);
+            return "Book Deleted Successfully!!";
+        }
+        return "Invalid Book Id!!";
     }
 }
