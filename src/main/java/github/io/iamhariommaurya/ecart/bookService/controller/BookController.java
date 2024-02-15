b2981bd1d0d2a7de31fba6dec57efb83f7a1dc98
harrymauray05 <harrymaurya05@gmail.com>
2024-02-15 23:00:09 +0530

    controller end point modified

--- src/main/java/github/io/iamhariommaurya/ecart/bookService/controller/BookController.java
+++ src/main/java/github/io/iamhariommaurya/ecart/bookService/controller/BookController.java
@@ -9,32 +9,32 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/v1")
+@RequestMapping(value = "/v1/book")
 public class BookController {
     @Autowired
     BookService bookService;
     //Add Book
-    @PostMapping(value = "/book")
+    @PostMapping
     public ResponseEntity<Book> saveBook(@RequestBody Book book){
         return ResponseEntity.ok(bookService.save(book));
     }
     //Get Books
-    @GetMapping(value = "/book")
+    @GetMapping
     public ResponseEntity<List<Book>> getBooks(){
         return ResponseEntity.ok(bookService.getAllBooks());
     }
     //Get Book by id
-    @GetMapping(value = "/book/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Book> getBookById(@PathVariable("id") long id){
         return ResponseEntity.ok(bookService.getBookByid(id));
     }
     //Update Book
-    @PutMapping(value = "/book/{id}")
+    @PutMapping(value = "/{id}")
     public ResponseEntity<Book> updateBook(@PathVariable("id") long id,@RequestBody Book book){
         return ResponseEntity.ok(bookService.updateBookById(id,book));
     }
     //Delete Book
-    @DeleteMapping(value = "/book/{id}")
+    @DeleteMapping(value = "/{id}")
     public ResponseEntity<String> deleteBookById(@PathVariable("id") long id){
         return ResponseEntity.ok(bookService.deleteBookByid(id));
     }
